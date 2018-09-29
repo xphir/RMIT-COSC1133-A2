a6157ef689d670b646e0c35fd0318380d8f79b93
Elliot Schot <s3530160@student.rmit.edu.au>
2018-09-29 14:01:33 +1000

    completed dynamic welcome menu
    added welcome_message(), create_folder_array() print_folder_array()

--- led_konfigurator.sh
+++ led_konfigurator.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 version=0.1
-leds=/sys/class/leds/
+LEDS_FOLDER=/sys/class/leds/
+declare -a arrFolderNames
 
 main() {
+    create_folder_array
     welcome_message
 }
 
@@ -11,8 +13,26 @@ welcome_message(){
     echo "Welcome to Led_Konfigurator!"
     echo "============================"
     echo "Please select an led to configure:"
-    echo "1. Loop goes here"
-    echo "Please enter a number (1-9) for the led to configure or quit:"
+    print_folder_array
+}
+
+create_folder_array(){
+    for folder in $LEDS_FOLDER*
+    do
+        folder=${folder%*/}
+        arrFolderNames=(${arrFolderNames[@]} "${folder##*/}")
+    done
+}
+
+print_folder_array(){
+    COUNTER=1
+    for FolderName in "${arrFolderNames[@]}"
+    do
+        echo "$COUNTER. $FolderName"
+        ((COUNTER++))
+    done
+    echo "$COUNTER. Quit"
+    echo "Please enter a number (1-$COUNTER) for the led to configure or quit:"
 }
 
 main $@
