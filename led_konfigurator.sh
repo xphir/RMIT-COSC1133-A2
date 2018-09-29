5525acaf08b2e6babab20af224f6059a72a4412d
Elliot Schot <s3530160@student.rmit.edu.au>
2018-09-29 12:53:52 +1000

    adding basic welcome menu

--- led_konfigurator.sh
+++ led_konfigurator.sh
@@ -4,9 +4,14 @@ version=0.1
 leds=/sys/class/leds/
 
 main() {
+    welcome_message
+}
 
-echo "Hello World"
-
+welcome_message(){
+    echo "Welcome to Led_Konfigurator!"
+    echo "============================"
+    echo "Please select an led to configure:"
+    echo "Please enter a number (1-9) for the led to configure or quit:"
 }
 
 main $@
